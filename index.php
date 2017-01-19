28c9eefd4121a1d2c60547d3046a304054d4c21e
Matt <m_dittmann@yahoo.com>
2017-01-18 23:31:35 -0800

    adding files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php echo "Hello, world!"; ?>
+<script>
+
+// var globalVar = "This is a global variable.";
+
+// var globalFunction = function () {
+// 	alert(globalVar);
+// 	globalVar = "Value has been modified";
+// };
+
+// globalFunction();
+
+// alert(globalVar);
+</script>
+
+<script>
+	window.alert("Here is some text");
+</script>
